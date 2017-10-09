8ef243cbdcb1df4510fd92d2e350c2a14e5bab99
ErikBoesen <me@erikboesen.com>
2017-10-09 16:03:16 -0400

    Switch to procedural layout

--- sch.py
+++ sch.py
@@ -9,58 +9,60 @@ import argparse
 
 CONFIG = expanduser('~') + '/.sch.yaml'
 
-class SCH:
+parser = argparse.ArgumentParser(description='Schoology in the terminal.')
+parser.add_argument('command', type=str, nargs='?', help='What you want to do (home, config).')
+parser.add_argument('id', type=int, nargs='?', help='ID to use in getting data.')
+parser.add_argument('--realm', nargs='?', help='Specify the realm to get a resource from.')
+
+args = parser.parse_args()
+
+cfg = {}
+# TODO: Handle config which exists, but lacks necessary fields
+# TODO: Make this more efficient
+try:
+    with open(CONFIG, 'r+') as f:
+        cfg = yaml.load(f.read())
+except FileNotFoundError:
     cfg = {}
-    def __init__(self):
-        # TODO: Handle config which exists, but lacks necessary fields
-        # TODO: Make this more efficient
-        with open(CONFIG, 'r+') as f:
-            self.cfg = yaml.load(f.read())
-        if not self.cfg:
-            print(c('No configuration detected. Run sch config to generate config file.', 'red'))
+if not cfg:
+    from getpass import getpass
 
-        # TODO: Unsure of naming of `api`
-        self.api = schoolopy.Schoology(self.cfg['key'], self.cfg['secret'])
-
-    def genconfig(self):
-        from getpass import getpass
-
-        print(c('Generating configuration for first run.', 'green'))
-        self.cfg = {}
-        self.cfg['key'] = input('API key: ')
-        self.cfg['secret'] = getpass('API secret: ')
-        print(self.cfg)
-        with open(CONFIG, 'w+') as f:
-            yaml.dump(self.cfg, f)
-
-    def home(self):
-        updates = self.api.get_feed()
-        users = []
-        # TODO: Switch to multi-get request once it's available
-        # TODO: Generalize loading system
-        for i,update in enumerate(updates):
-            sys.stdout.write('\r%s %s/%s' % ('/-\\|'[i%4], i, len(updates)))
-            sys.stdout.flush()
-            users.append(self.api.get_user(update.uid))
-
-        sys.stdout.write('\r')
-        for user,update in zip(users,updates):
-            print(c(user.name_display, 'red'), end='')
-            print(' / ' + update.body[:40].replace('\r\n', ' ').replace('\n', ' ') + '... / ', end='')
-            print(c('%dL' % update.likes, 'yellow'))
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Schoology in the terminal.')
-    parser.add_argument('command', type=str, nargs='?', help='What you want to do (home, config).')
-
-    args = parser.parse_args()
-
-    sch = SCH()
+    print(c('Generating configuration for first run.', 'green'))
+    cfg = {}
+    cfg['key'] = input('API key: ')
+    cfg['secret'] = getpass('API secret: ')
+    with open(CONFIG, 'w+') as f:
+        yaml.dump(cfg, f)
 
 
-    if not args.command or args.command == 'home':
-        sch.home()
-    elif args.command.startswith('config'):
-        sch.genconfig()
-    else:
-        print('Unknown command %s.' % args.command)
+# TODO: Unsure of naming of `api`
+api = schoolopy.Schoology(cfg.get('key'), cfg.get('secret'))
+
+if not args.command or args.command == 'home':
+    updates = api.get_feed()
+    users = []
+    # TODO: Switch to multi-get request once it's available
+    # TODO: Generalize loading system
+    for i,update in enumerate(updates):
+        sys.stdout.write('\r%s %s/%s' % ('/-\\|'[i%4], i, len(updates)))
+        sys.stdout.flush()
+        users.append(api.get_user(update.uid))
+
+    sys.stdout.write('\r')
+    for user,update in zip(users,updates):
+        print(c(user.name_display, 'red'), end='')
+        print(' / ' + update.body[:40].replace('\r\n', ' ').replace('\n', ' ') + '... / ', end='')
+        print(c('%dL' % update.likes, 'yellow'))
+elif args.command == 'update':
+    update_id = args.id
+    realm_type = args.realm[0]
+    realm_id = args.realm[1]
+    if realm_type == 'section':
+        update = api.get_update(update_id, section_id=realm_id)
+    elif realm_type == 'group':
+        update = api.get_update(update_id, group_id=realm_id)
+    elif realm_type == 'user':
+        update = api.get_update(update_id, user_id=realm_id)
+    user = api.get_user(update.uid)
+else:
+    print('Unknown command %s.' % args.command)
