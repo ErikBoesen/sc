599ab79f6ba7566945b63bc4f940fd6727af43fa
ErikBoesen <me@erikboesen.com>
2017-10-11 15:55:17 -0400

    Form into REPL structure

--- sch.py
+++ sch.py
@@ -5,16 +5,10 @@ import yaml
 from termcolor import colored as c
 from os.path import expanduser
 import sys
-import argparse
+import re
+from datetime.datetime import timestamp
 
 CONFIG = expanduser('~') + '/.sch.yaml'
-
-parser = argparse.ArgumentParser(description='Schoology in the terminal.')
-parser.add_argument('command', type=str, nargs='?', help='What you want to do (home, config).')
-parser.add_argument('id', type=int, nargs='?', help='ID to use in getting data.')
-parser.add_argument('--realm', nargs=2, help='Specify the realm to get a resource from.')
-
-args = parser.parse_args()
 
 cfg = {}
 # TODO: Handle config which exists, but lacks necessary fields
@@ -38,48 +32,59 @@ if not cfg:
 # TODO: Unsure of naming of `api`
 api = schoolopy.Schoology(cfg.get('key'), cfg.get('secret'))
 
+def date(timestamp):
+    return fromtimestamp(timestamp).strftime('%Y/%m/%d, %H:%M:%S')
 
-def update_identifier(update):
-    if update.get('section_id'):
-        return 'S%s' % update.section_id
-    elif update.get('group_id'):
-        return 'G%s' % update.group_id
-    elif update.get('user_id'):
-        return 'U%s' % update.user_id
-    elif update.get('building_id'):
-        return 'B%s' % update.building_id
+def display(data):
+    if isinstance(data, list): # data is a list of objects to show
+        if isinstance(data[0], schoolopy.Update):
+            users = []
+            # TODO: Switch to multi-get request once it's available
+            # TODO: Generalize loading system
+            for i, update in enumerate(data):
+                sys.stdout.write('\r%s %s/%s' % ('/-\\|'[i % 4], i, len(data)))
+                sys.stdout.flush()
+                users.append(api.get_user(update.uid))
 
+            sys.stdout.write('\r')
+            for user, update in zip(users, data):
+                print(c(user.name_display, 'red'), end='')
+                print(' / ' + update.body[:40].replace('\r\n', ' ').replace('\n', ' ') + '... / ', end='')
+                print(c('%dL' % update.likes, 'yellow'))
+        elif isinstance(data[0], schoology.Group):
+            pass
+        elif isinstance(data[0], schoology.Section):
+            pass
+    else: # data is scalar object
+        if isinstance(data, schoology.Update):
+            user = api.get_user(data.uid)
+            print('By: %s' % user.name_display)
+            print('Date: %s' % date(user.created))
+            print(data.body)
+        elif isinstance(data, schoology.Group):
+            pass
+        elif isinstance(data, schoology.User):
+            pass
 
-if not args.command or args.command == 'home':
-    updates = api.get_feed()
-    users = []
-    # TODO: Switch to multi-get request once it's available
-    # TODO: Generalize loading system
-    for i, update in enumerate(updates):
-        sys.stdout.write('\r%s %s/%s' % ('/-\\|'[i % 4], i, len(updates)))
-        sys.stdout.flush()
-        users.append(api.get_user(update.uid))
+shown = api.get_feed()
+display(shown)
 
-    sys.stdout.write('\r')
-    for user, update in zip(users, updates):
-        print(c(user.name_display, 'red'), end='')
-        print(' / ' + update.body[:40].replace('\r\n', ' ').replace('\n', ' ') + '... / ', end='')
-        print(c('%dL' % update.likes, 'yellow'), end='')
-        print(' / %s/%s' % (update.id, update_identifier(update)))
-elif args.command == 'update':
-    update_id = args.id
-    realm_type = args.realm[0]
-    realm_id = args.realm[1]
-    print(update_id, realm_type, realm_id)
-    if realm_type == 'section':
-        update = api.get_update(update_id, section_id=realm_id)
-    elif realm_type == 'group':
-        update = api.get_update(update_id, group_id=realm_id)
-        print(update)
-    elif realm_type == 'user':
-        update = api.get_update(update_id, user_id=realm_id)
-    elif realm_type == 'building':
-        update = api.get_update(update_id, building_id=realm_id)
-    user = api.get_user(update.uid)
-else:
-    print('Unknown command %s.' % args.command)
+while True:
+    try:
+        rawcmd = input('> ')
+        try:
+            cmd = re.search(r'^(\w+)', rawcmd).group(0)
+            content = rawcmd[len(cmd)+1:].split(' ')
+        except AttributeError:
+            cmd = None
+            content = None
+        print('RECIEVED: %s, %s' % (cmd, content))
+
+        if cmd == 'view':
+            display(shown[int(content[0])])
+        elif cmd == 'home':
+            pass
+
+    except EOFError:
+        print()
+        sys.exit(0)
